f1ef3bc0031b2605485c103b050247a9b42f6a9b
peyanshin <peyanshin@yandex.ru>
2026-03-17 03:38:42 +0300

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,8 @@
 import allure
 
 from pages.base_page import BasePage
-
 from locators.main_page_locators import *
 from locators.ingredient_page_locators import *
-
 
 class MainPage(BasePage):
     @allure.step("Нажать кнопку Лента заказов")
@@ -16,27 +14,34 @@ class MainPage(BasePage):
         self.click_on_element(MainPageLocators.CON_BUTTON)
 
     @allure.step("Проверить, что текущий URL содержит ожидаемый домен")
-    def check_current_url_contains(self, expected_domain):
-        self.assert_url_contains(expected_domain)
+    def check_current_url_contains(self, expected_domain: str) -> bool:
+        current_url = self.driver.current_url
+        return expected_domain in current_url
 
     @allure.step("Нажать на булку")
     def click_bread_ingredient_button(self):
         self.click_on_element(MainPageLocators.BRE_DAD_ITEM)
-        self.find_element(IngredientPageLocators.ING_DET_TEXT)
-        self.see_element(IngredientPageLocators.ING_DET_TEXT)
 
-    @allure.step("Закрыть окно и убедиться, что оно закрылось")
+    @allure.step("Проверить видимость окна Детали ингредиента")
+    def is_ingredient_details_visible(self):
+        self.see_element(IngredientPageLocators.ING_DET_TEXT)
+        return True
+
+    @allure.step("Проверить скрытие окна Детали ингредиента")
+    def is_ingredient_details_hidden(self):
+        self.hide_element(IngredientPageLocators.MODAL_CONTAINER)
+        return True
+
+    @allure.step("Закрыть окно Детали ингредиента")
     def close_window(self):
         self.click_on_element(IngredientPageLocators.ING_CLOSE_BUTTON)
-        self.hide_element(IngredientPageLocators.MODAL_CONTAINER)
 
     @allure.step("Перенос булки в корзину")
     def drag_and_drop_bread(self):
-        self.drag_and_drop_element(MainPageLocators.BRE_DAD_ITEM,MainPageLocators.BASKET_AREA)
+        self.drag_and_drop_element(MainPageLocators.BRE_DAD_ITEM, MainPageLocators.BASKET_AREA)
 
-    @allure.step("Проверить, что значение соответствует ожидаемому")
-    def should_have_correct_answer(self, expected_text: str):
+    @allure.step("Получить текст счётчика ингредиента")
+    def get_ingredient_counter_text(self) -> str:
+        self.wait_for_element_visibility(MainPageLocators.BRE_COUNTER)
         element = self.find_element(MainPageLocators.BRE_COUNTER)
-        actual_text = element.text
-        assert actual_text == expected_text, (f"Значение {expected_text} не соответствует ожидаемому {actual_text}.")
-   
+        return element.text
